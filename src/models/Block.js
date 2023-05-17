154e76afd9e078427872e40d3adfded36f36012c
Annxxnn <132678447+Annxxnn@users.noreply.github.com>
2023-05-17 22:32:24 +0800

    update2

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,14 +1,36 @@
+import sha256 from 'crypto-js/sha256.js'
+
+export const DIFFICULTY = 2
+
 class Block {
   // 1. 完成构造函数及其参数
-  /* 构造函数需要包含
 
-  */
-  constructor(blockchain,previousHash,height,hash){
+  constructor(blockchain,previousHash,height,hash,nonce){
     this.blockchain=blockchain
     this.previousHash=previousHash
     this.height=height
     this.hash=hash
+    this.nonce=nonce
+  }
+// 验证区块是否符合挖矿难度要求
+
+// 添加isValid函数
+isValid() {
+  const hash=this.calculateHash()
+  return hash.substring(0,DIFFICULTY) == '0'.repeat(DIFFICULTY)
+}
+
+// 添加更新Nonce函数
+  setNonce(nonce) {
+    this.nonce=nonce
+  }
+
+// 添加更新Hash函数
+  calculateHash(){
+    return sha256(
+      this.previousHash+this.height+this.nonce).toString()
   }
 }
 
 export default Block
+
--- src/validators/utils.js
+++ src/validators/utils.js
@@ -1,3 +1,22 @@
-export const validateHash = () => {
-    
+import sha256 from 'crypto-js/sha256.js'
+
+export const validateHash = () => {}
+//定义一个参数接收block
+export const calcNonce = (block) => {
+  console.log(`calc nonce of block ${block.height} `)
+  //获取时间戳
+  const start = new Date().getTime()
+  //计算hash的次数
+  let calcTimes = 0
+   //先看block是否符合难度要求，不符合就calcTime加1
+  while (!block.isValid()) {
+    block.setNonce(sha256(new Date().getTime().toString()).toString())
+    calcTimes++
+  }
+  const end = new Date().getTime()
+  // 计算每个区块的挖矿时间
+  console.log(
+    `calc nonce cost ${(end - start) / 1000}s, try ${calcTimes} times`,
+  )
+  return block
 }
